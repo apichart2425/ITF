9f9b38391318c7745ebc171b41a85babef39208f
Niti Jira <60070041@kmitl.ac.th>
2017-11-18 22:19:44 +0700

    add backend and sql file

---
+++ FRONT/HTML/connect.php
@@ -0,0 +1,12 @@
+<?php 
+$dbServername = 'localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'war';
+
+
+$conn = @mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName) or die("Unable to connect");
+mysqli_query($conn, "SET NAMES UTF8");
+echo "Great Work" . "<br>";
+
+ ?>
